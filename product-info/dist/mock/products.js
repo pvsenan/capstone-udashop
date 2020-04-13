5453fd88db25c209ecc508567029b6bce63856cc
Prabhendu Senan <pvsenan@gmail.com>
2020-04-13 18:51:45 +0200

    initial commit product-info service

---
+++ product-info/dist/mock/products.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.storeProducts = [
+    {
+        sku: "7314878358006",
+        name: "Kanelbullar 450g Skogaholms",
+        description: "Bullar med krämig kanelfyllning och toppade med pärlsocker. Passar till fikastunden, på utflykten och som mellanmål med ett glas mjölk.",
+        price: 21
+    },
+    {
+        sku: "7318690129097",
+        name: "Minimajskakor Gräddfil & Lök Ekologisk 50g ICA I love eco",
+        description: "Energi (kcal) 400 kcal, Energi (kJ) 1700 kJ, Fett 5.90 g, Varav mättat fett 0.80 g, Kolhydrater 77 g, Varav socker 3.10 g, Fiber 3.40 g, Protein 8 g, Salt 2.10 g",
+        price: 15
+    },
+    {
+        sku: "7310521053106",
+        name: "Brago 225g Göteborgs",
+        description: "<strong>Information från leverantör</strong><br />Brago är svenska folkets vardagsfavorit bland kexen. Det spröda mördegskexen med den milda vaniljsmaken har funnits sedan 1953. Brago finns också som bondgårds-, siffer- och bokstavskex – för dig som tycker om att leka och som inte alltid vill ta livet på dess fulla allvar.",
+        price: 17
+    },
+    {
+        sku: "7300400115636",
+        name: "Runda Kanel 330g Wasa",
+        description: "Wasa Kanel är ett litet sprött knäckebröd som är bakat med vete och kryddat med kanel. Passar att äta som det är eller med ett gott smörgåspålägg.",
+        price: 20
+    },
+    {
+        sku: "7318690113621",
+        name: "Halländskt Bondbröd 1kg ICA Selection",
+        description: "VETEMJÖL,RÅGMJÖL,vatten,skållat VETEMJÖL och RÅGMJÖL,surdeg(RÅGMJÖL,VETEMJÖL,vatten,KORNMALT), sirap,,rapsolja,jäst,salt.",
+        price: 22
+    },
+    {
+        sku: "7318690142638",
+        name: "Majskakor ost 130g ICA",
+        description: "Majs 75%, rapsolja, kryddblandning 10% (maltodextrin, vasslepulver (MJÖLK), salt, ostpulver (MJÖLK), socker, aromer, lökpulver, surhetsreglerande medel (E330)). Kan innehålla spår av sesamfrön och soja.",
+        price: 44
+    }
+];
+//# sourceMappingURL=products.js.map
